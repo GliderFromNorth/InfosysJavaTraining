b387e07c757a6618f28ea8a374cfbc810d4adc88
Saurav Jha <jhasaurav@outlook.com>
2022-03-30 14:43:15 +0530

    getter and setter

---
+++ Java11Essenstials/src/main/java/com/Infy/GettersAndSetters/TestGetterAndSetter.java
@@ -0,0 +1,17 @@
+package com.Infy.GettersAndSetters;
+
+public class TestGetterAndSetter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Customer customer = new Customer();
+		
+		customer.customerName = "Ronny";
+		System.out.println("Customer Name: "+customer.getCustomerName());
+		
+		
+
+	}
+
+}
