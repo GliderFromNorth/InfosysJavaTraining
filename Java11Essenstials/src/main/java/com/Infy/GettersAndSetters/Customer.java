e6618cbd42fbd7997c698467e6b5883b11cae3dd
unknown <info29spring102@iiht.tech>
2022-03-30 13:59:08 +0530

    java Beyond basics

--- Java11Essenstials/src/main/java/com/Infy/GettersAndSetters/Customer.java
+++ Java11Essenstials/src/main/java/com/Infy/GettersAndSetters/Customer.java
@@ -1,5 +1,16 @@
 package com.Infy.GettersAndSetters;
 
 public class Customer {
+	String customerName;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
 
 }
